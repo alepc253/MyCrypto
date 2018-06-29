0e122029b55697c0f46b9d9b23a999de5fcec87a
Ilan <ilan@rsk.co>
2018-06-29 17:48:39 -0300

    RSK as main networks.

--- common/components/NetworkSelector/NetworkSelector.tsx
+++ common/components/NetworkSelector/NetworkSelector.tsx
@@ -18,7 +18,7 @@ import {
 import NetworkOption from './NetworkOption';
 import './NetworkSelector.scss';
 
-const CORE_NETWORKS = ['ETH', 'ETC', 'Ropsten', 'Kovan', 'Rinkeby'];
+const CORE_NETWORKS = ['RSK', 'RSK_TESTNET'];
 
 interface OwnProps {
   openCustomNodeModal(): void;
